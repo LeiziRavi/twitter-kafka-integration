ea2269c4df62d372ad4f0305e13488d234d638b2
LeiziRavi <ravi4582@outlook.com>
2022-04-24 01:36:21 +0530

    Create Spring Starter Project kafka-twitter-integration
    Spring boot starter project has been created with Spring Kafka, Spring Kafka Streams and Lombok as set of dependencies.

---
+++ kafka-twitter-integration/src/main/java/com/example/twitter/KafkaTwitterIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTwitterIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTwitterIntegrationApplication.class, args);
+	}
+
+}
---
+++ kafka-twitter-integration/src/test/java/com/example/twitter/KafkaTwitterIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTwitterIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
